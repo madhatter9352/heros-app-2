111b8a1215d701abb17f768bc1e6195aede07085
Lazaro Camacho <lachy9352@gmail.com>
2022-03-03 09:01:24 -0400

    Annadiendo pantalla para la busqueda

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -2,6 +2,32 @@ import React from 'react';
 
 export const SearchScreen = () => {
   return (
-    <div>SearchScreen</div>
+    <div>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-sm-6">
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                id="search"
+                placeholder="Search"
+                name="search"
+              />
+
+              <button 
+              type="submit" 
+              className="btn btn-primary mt-1"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
